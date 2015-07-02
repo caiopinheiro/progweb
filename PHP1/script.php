57864ccecc9ed21eb620a7d61ac151e33d605a41
Caio <pinheiro.caiof@gmail.com>
2015-07-02 12:18:03 -0400

    correção de arquivo enviado

--- PHP1/script.php
+++ PHP1/script.php
@@ -1,14 +1,14 @@
 <?php 
 
 
-$nome = $_POST("nome");
-$sexo = $_POST("sexo");
-$comentario = $_POST("comentario");
+$nome = $_POST["nome"];
+$sexo = $_POST["sexo"];
+$comentario = $_POST["comentario"];
+$genero = array(1=>"Masculino",2=>"Feminino",3=>"Outros");
 
-echo $nome;
-echo $sexo;
-echo $nome;
-
+var_dump($nome) ;
+var_dump($genero[$sexo]); 
+var_dump($comentario);
 
 
 
